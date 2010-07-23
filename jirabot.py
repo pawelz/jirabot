f208371fb2731f461cab8f4bf53d6a90219de36b
Paweł Zuzelski <pawelz@pld-linux.org>
2010-07-23 15:00:26 +0200

    !display

--- jirabot.py
+++ jirabot.py
@@ -13,16 +13,23 @@ import re
 import os
 import sys
 import suds
+import time
 
 from pyjira import soap
 from pyjira import jira
 from pyjira import jiraError
 
+def login():
+	return jira.Jira(ekg.config["jirabot:url"], ekg.config["jirabot:username"], ekg.config["jirabot:password"])
+
+r = login()
+rx = re.compile("%s-[0-9]+" % ekg.config["jirabot:project"])
+rl = re.compile("!list")
+rd = re.compile("!display ([A-Z]+-[0-9]+)")
+rp = r.getProject(ekg.config["jirabot:project"])
+
 def initialize(name, args):
-	r = jira.Jira(ekg.config["jirabot:url"], ekg.config["jirabot:username"], ekg.config["jirabot:password"])
-	rx = re.compile("%s-[0-9]+" % ekg.config["jirabot:project"])
-	rl = re.compile("!list")
-	rp = r.getProject(ekg.config["jirabot:project"])
+	r = login()
 
 def print_config(name, args):
 	ekg.echo("JIRA url:      %s" % ekg.config["jirabot:url"])
@@ -32,19 +39,31 @@ def print_config(name, args):
 	ekg.echo("IRC  channel:  %s" % ekg.config["jirabot:channel"])
 
 def messageHandler(session, uid, type, text, stime, ignore_level):
+	r = login()
+
+	mx = rd.search(text)
+	if (mx):
+		i = r.getIssueByKey(mx.group(1))
+		for line in i.display().splitlines():
+			ekg.command("/msg %s %s" % (uid, line))
+			time.sleep(.5)
+		return
+
 	mx = rx.search(text)
 	if (mx):
 		ekg.echo("Issue: "+mx.group(0))
 		try:
 			i = r.getIssueByKey(mx.group(0))
-			ekg.command("/msg %s %s: %s" % (ekg.config["jirabot:channel"], mx.group(0), i.raw.summary))
+			ekg.command("/msg %s %s: %s" % (uid, mx.group(0), i.raw.summary))
 		except jiraError.IssueNotFound:
-			ekg.command("/msg %s %s: Issue not found." % (ekg.config["jirabot:channel"], mx.group(0)))
+			ekg.command("/msg %s %s: Issue not found." % (uid, mx.group(0)))
+		return
 
 	mx = rl.search(text)
 	if (mx):
 		i = rp.getIssues()
-		ekg.command("/msg %s %s" % (ekg.config["jirabot:channel"], " ".join([s.raw.key for s in i])))
+		ekg.command("/msg %s %s" % (uid, " ".join([s.raw.key for s in i])))
+		return
 
 ekg.handler_bind("protocol-message-received", messageHandler)
 
